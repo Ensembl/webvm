05e1f82d1ebc048bd3c899152c3f2231ca279d8c
Matthew Astley <huncat$mca@t8o.org>
2012-05-14 17:23:49 +0100

    discover ${ENV} substitution in conf.
    this makes sanity easier.
    
    move config directives out of the wrapper;
    tell Apache where the Git checkout & tmpdir are;
    no longer need the logs/ symlink;
    no longer ../ hack (579a0094), and fix htdocs;
    remove hardwiring (59c89a32) from my config chunk;
    refer to logs/ via ${WEBTMPDIR}

--- setup.user.sh
+++ setup.user.sh
@@ -16,17 +16,11 @@ fi
 
 git clone "$WEBVM" "$WEBROOT/$FOR_USER"
 
-# XXX:LOCALMOD symlink ServerRoot/logs to user's tmpdir.
-#
 # We use that tmpdir for logs, pids and any other tmpfiles to ease
 # webteam clone of a server - they exclude it and make a blank on on
 # the target.
-#
-# There may be a neater way to point there (like "apache2 -c"?) but
-# this is good enough for now.
 #
 # Some files stored here insist on being on local disk.
 
 LOGDIR="$WEBROOT/tmp/$FOR_USER/logs"
 mkdir -p -v "$LOGDIR"
-ln -s -v "$LOGDIR" "$WEBROOT/$FOR_USER/ServerRoot/logs"
