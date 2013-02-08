57b35ad1bd25f281e82422b7bd91f95e75989edb
Matthew Astley <mca@sanger.ac.uk>
2013-02-08 17:23:48 +0000

    rework the mult-version Otter Server builder
    bugfix absent versions;
    build success is quiet;
    tell of versions left over from previous builds

--- otter_server_build.sh
+++ otter_server_build.sh
@@ -5,6 +5,8 @@
 #
 # cd to anywhere, or the ensembl-otter clone you would use as source.
 # Run this file by its path within the webvm clone you want updated.
+#
+# Expects relevant bits of team_tools.git to be on PATH.
 
 set -e
 unset CDPATH
@@ -12,9 +14,11 @@ unset CDPATH
 
 # Take server code from?
 REPO_URL=intcvs1:/repos/git/anacode/ensembl-otter.git
+REPO_NAME=intcvs1
 if [ -d ".git" ] && [ -f "dist/conf/version_major" ]; then
     # Looks like a dev copy.  Clone that instead.
     REPO_URL="$PWD"
+    REPO_NAME=workdir
 fi
 
 : ${BUILD_VSNS:=humpub-branch-71 humpub-branch-72 master}
@@ -47,15 +51,45 @@ echo "Going to build to $OTT_DEST/ versions ($BUILD_VSNS)
       cloned from $REPO_URL"
 
 printf '\nCloning ... '
-git clone -q $REPO_URL "$EOCLONE"
+git clone -o $REPO_NAME -q $REPO_URL "$EOCLONE"
 echo
 cd "$EOCLONE"
-git checkout -b server_build_tmp
+
+# detached head
+git checkout -q $( git log -1 --format=%H HEAD )
+
+sedent(){
+    sed -e 's/^/  /'
+}
 
 for VSN in $BUILD_VSNS; do
     printf '\n\n\nBuilding for %s\n' "$VSN"
     cd "$EOCLONE"
-    git reset --hard origin/$VSN || \
-        git reset --hard 
-    otter_nfswub="$OTT_DEST" otterlace_build --server-only
+    rev=$REPO_NAME/$VSN
+    if git reset -q --hard $rev; then
+        git log -1 --format=fuller --decorate | sedent
+        export otter_nfswub="$OTT_DEST"
+        logfile="$( mktemp --tmpdir=$EOCLONE build-log.$VSN.XXXXXX )"
+        if otterlace_build --server-only > $logfile 2>&1; then
+            echo OK
+        else
+            cat $logfile | sedent
+            echo $VSN failed >&2
+            exit 2
+        fi
+    else
+        echo "Skip: build for $VSN because $rev not found"
+    fi
 done
+
+
+stale=$(
+    find $OTT_DEST/lib/otter $OTT_DEST/cgi-bin/otter \
+        -maxdepth 1 ! -newer $EOCLONE/.git/config -ls 2>&1
+)
+if [ -n "$stale" ]; then
+    printf '\n\nW: stale server versions exist and were not touched\n'
+    printf '%s' "$stale" | sedent
+else
+    echo All server versions updated
+fi
