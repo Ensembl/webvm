17f0f1b19599dc261d1a16ef1cae53a43743e0b2
Matthew Astley <mca@sanger.ac.uk>
2013-09-11 15:55:18 +0100

    setup/.mca.APACHECTL.sh - copy+rename from branch mca/deskpro 4b517aeb
    put the file on master so it can be seen and shared
    branch mca/deskpro shall have a symlink to provide it at setup/APACHECTL.sh

---
+++ setup/.mca.APACHECTL.sh
@@ -0,0 +1,36 @@
+
+  PATH=~/bin:/software/bin:/usr/bin:/bin
+
+  # OSperl is too old and /software/perl-* exists here
+  PATH=/software/perl-5.12.2/bin:$PATH
+  OTTER_PERL_EXE=$(which perl)
+
+  # locks & logs in here; must mkdir it first
+  WEBTMPDIR=/var/tmp/$USER.apache2/8002
+  mkdir -p $WEBTMPDIR/logs
+
+  BUILT=$WEBDIR/httpd
+
+  # needed also for tools/*
+  APACHE2_MODS=$BUILT/modules
+  APACHE2_SHARE=$BUILT
+
+  # do this early to preserve pattern below
+  LD_LIBRARY_PATH=$BUILT/lib
+
+  # pass only the necessary environment
+  APACHE2=$( echo env -i \
+      PATH=$PATH \
+      USER=$USER \
+      WEBDIR=$WEBDIR \
+      WEBTMPDIR=$WEBTMPDIR \
+      LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
+      APACHE2_MODS=$APACHE2_MODS \
+      APACHE2_SHARE=$APACHE2_SHARE \
+      OTTER_PERL_EXE=$OTTER_PERL_EXE \
+      $BUILT/bin/httpd
+  )
+#  printf "[d] APACHE2=%s\n" "$(echo $APACHE2 | sed -e 's/ /\n\t/g' )"
+#  printf "[d] Using Perl=%s\n" "$(which perl)"
+
+WEBDEFS=deskpro,devel
